3c2ee0f784fa4037f78092e17600aa127cafc419
HaNaK0 <hampus@hampushuledal.com>
2022-01-03 20:19:27 +0100

    cahnged text on page

--- src/app.rs
+++ src/app.rs
@@ -5,8 +5,8 @@ pub fn app() -> Html {
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <h1>{ "This will be a portfolio!" }</h1>
+            <span class="subtitle">{ "built with yew" }<i class="heart" /></span>
         </main>
     }
 }
