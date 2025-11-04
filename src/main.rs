0925e42cd76d154e60ec588d62c144595d93f96c
HaNa <hampus@hampushuledal.com>
2025-11-04 18:36:13 +0100

    first steps not tested due to issues in termux

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let on_click = {
+        let counter = counter.clone();
+        move |_| { 
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {on_click}> {"+1"} </button>
+            <p> { *counter } </p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
