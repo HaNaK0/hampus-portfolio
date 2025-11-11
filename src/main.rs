8b5b08a2dacd187c146d496702d58aaa19e75174
HaNaK0 <hampus@hampushuledal.com>
2025-11-11 12:26:58 +0100

    add leptos base
    add leptos to cargo.toml
    add base leptos code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use leptos::prelude::*;
+
 fn main() {
-    println!("hello world");
+    mount_to_body(App);
+}
+
+#[allow(non_snake_case)]
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+    let double_count = move || count.get() * 2;
+
+    view! {
+        <button 
+            on:click=move |_| *set_count.write() += 10
+            style="position:absolute"
+            style:left=move || format!("{}px", count.get() + 100)
+            style:backgound-color=move || format!("rgb({}, 100, 100)", count.get())
+            style:max-width="400px"
+            style=("--columns", move || count.get().to_string())
+        >
+            "click me: " {count}
+        </button>
+
+        <br/>
+
+        <ProgressBar progress=count/>
+        <ProgressBar progress=Signal::derive(double_count)/>
+
+        <p> 
+            "Double count: "
+            {double_count}
+        </p>
+    }
+}
+
+#[allow(non_snake_case)]
+#[component]
+fn ProgressBar(
+    #[prop(default = 100)]
+    max: u16,
+    #[prop(into)]
+    progress: Signal<i32>
+) -> impl IntoView {
+    view! {
+        <progress
+            max=max
+            value=progress
+        />
+        <br/>
+    }
 }
