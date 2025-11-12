be4a425ec7dc24ad1548e783c7f16c06b231c73d
HaNaK0 <hampus@hampushuledal.com>
2025-11-12 15:17:52 +0100

    add logging to console
    add log crate
    add console_log crate
    enable logging in main function
    move error panic hook from app to main

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use leptos::{
     html::{h1, p},
     prelude::*,
 };
+use log::info;
 
 fn main() {
+    // makes sure panics shows up as readble errors in the web console
+    console_error_panic_hook::set_once();
+    console_log::init_with_level(log::Level::Trace).expect("Failed to start logger");
+    info!("Logger Attached");
     mount_to_body(App);
 }
 
 #[allow(non_snake_case)]
 #[component]
 fn App() -> impl IntoView {
-    // makes sure panics shows up as readble errors in the web console
-    console_error_panic_hook::set_once();
-
     (
         h1().child("Hampus portfolio"),
         p().child("This is just a beginning written in rust using leptos"),
